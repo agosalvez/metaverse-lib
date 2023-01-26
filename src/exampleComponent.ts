3357217879cddd20f420a108142eb9d752d13b4a
Adrián Gosálvez <adrian.gosalvez@gmail.es>
2023-01-26 12:10:54 +0100

    add rabbitmq lib and code

--- src/exampleComponent.ts
+++ src/exampleComponent.ts
@@ -1,10 +1,35 @@
+import * as amqp from 'amqplib'
+import { Channel, Connection } from 'amqplib'
+
 /**
  * @public
  * @returns Some example component
  */
 // @Component('exampleComponent')
 export class ExampleComponent {
-  constructor(public testing: boolean) {
+  private connection: Promise<Connection>
+  private channel: Channel
+  private URI: string
+
+  constructor(public host: string, public user: string, public pass: string, public port: number, public queue: string) {
+    this.URI = 'amqp://' + user+':' + pass + '@' + host + ':' + port.toString()
+    console.info(this.URI)
+    this.initialize(queue)
+  }
+
+  initialize(queue: string) {
+    return amqp.connect(this.URI)
+              .then(conn => conn.createChannel())
+              .then(channel => {
+                this.channel = channel
+                return this.channel.assertQueue(this.queue)
+              })
+              .then(q => this.queue = q.queue)
+              .catch(err => console.error(err.stack))
+  }
+
+  public async sendMsg(msg: string) {
+    this.channel.sendToQueue(this.queue, Buffer.from(msg))
   }
 }
 
