a31711702b85b69bbe10d0ca3be198c4cd1e5dbf
Adrián Gosálvez <adrian.gosalvez@gmail.es>
2023-01-26 16:11:56 +0100

    ws 0

--- src/exampleComponent.ts
+++ src/exampleComponent.ts
@@ -1,38 +1,12 @@
-import * as amqp from 'amqplib'
-import { Channel, Connection } from 'amqplib'
-
 /**
  * @public
  * @returns Some example component
  */
 // @Component('exampleComponent')
 export class ExampleComponent {
-  private connection: Promise<Connection>
-  private channel: Channel
-  private URI: string
-
-  constructor(public host: string, public user: string, public pass: string, public port: number, public queue: string) {
-    this.URI = 'amqp://' + user+':' + pass + '@' + host + ':' + port.toString()
-    console.info(this.URI)
-    this.queue = queue
-  }
-
-  async initialize() {
-    return amqp.connect(this.URI)
-              .then(conn => conn.createChannel())
-              .then(channel => {
-                this.channel = channel
-                return this.channel.assertQueue(this.queue)
-              })
-              .then(q => this.queue = q.queue)
-              .catch(err => console.error(err.stack))
-  }
-
-  public async sendMsg(msg: string) {
-    this.channel.sendToQueue(this.queue, Buffer.from(msg))
+  constructor(public testing: boolean) {
   }
 }
-
 
 /**
  * @public
@@ -47,5 +21,5 @@ export const constant = 'constant'
  * This is a public function that does something
  */
 export function someFn() {
-  return 'some-value-modified'
+  return 'version ws 1 reset'
 }
